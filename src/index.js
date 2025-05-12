fcef6abd0010232ed21d2d01c01b541a7756830d
hge2ne <hge2ne@gmail.com>
2025-05-12 22:27:55 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from "react-redux";
 import store from './store.js'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'  
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={QueryClient}>
     <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
     </Provider>
+    </QueryClientProvider>
+   
   </React.StrictMode>
 );
 
